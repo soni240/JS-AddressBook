40ee90bf42c1102f8f5618b92b53e371a6d0603d
soni240 <s.fegade21@gmail.com>
2022-06-19 22:52:48 +0530

    [Gunavanti]sorting entry in book alphabetically name

--- uc10.js
+++
@@ -1,2 +0,0 @@
-//UC10-Getting count of people living in a city/state
-console.log("Number of people from that city is: "+peopleFromCity.length);
---
+++ uc11.js
@@ -0,0 +1,3 @@
+//UC11-Sorting the address book according to person's name
+let sortedArray = addressArr.sort(a=>a.firstName);
+console.log(sortedArray);
