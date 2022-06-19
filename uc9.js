fc481756ff5580b6c4954c0c4d6c941780ed0248
soni240 <s.fegade21@gmail.com>
2022-06-19 22:50:37 +0530

    [Gunavanti]search result show count by cityorstate

--- uc9.js
+++
@@ -1,8 +0,0 @@
-//UC9-Viewing person by city/state
-function ReturnCityMap(a)
-{
-    return a.city+ "->"+a.firstName;
-}
-let addressCityMap= addressArr.map(ReturnCityMap);
-console.log("Viewing the people by their city")
-console.log(addressCityMap);
