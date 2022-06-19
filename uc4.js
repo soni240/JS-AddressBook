51bb19d629ec546355c014e3bfb4a03b3ad93b61
soni240 <s.fegade21@gmail.com>
2022-06-19 22:36:51 +0530

    [Gunavanti]deleting name

--- uc4.js
+++
@@ -1,7 +0,0 @@
-//UC4-Searching an address by using the firstname
-function SearchName(addressbook)
-{
-    if(addressbook.firstName=="Amit") return addressbook;
-}
-let searchResult =addressArr.find(SearchName);
-console.log("Result after searching "+searchResult);
